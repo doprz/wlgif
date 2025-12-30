bddf7042aedfa7b182365db9ff36c3fb815ade74
doprz <52579214+doprz@users.noreply.github.com>
2025-12-29 22:14:13 -0600

    refactor(nix): use flake-parts

--- flake.nix
+++ flake.nix
@@ -3,100 +3,108 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
+    inputs@{
+      flake-parts,
       crane,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-        craneLib = crane.mkLib pkgs;
+      imports = [ ];
 
-        # Runtime dependencies that wlgif calls via Command::new
-        runtimeDeps = with pkgs; [
-          slurp
-          wf-recorder
-          ffmpeg
-        ];
+      perSystem =
+        {
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          craneLib = crane.mkLib pkgs;
 
-        # Common arguments can be set here to avoid repeating them later
-        # Note: changes here will rebuild all dependency crates
-        commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
-          strictDeps = true;
-
-          nativeBuildInputs = [
-            pkgs.makeWrapper
+          # Runtime dependencies that wlgif calls via Command::new
+          runtimeDeps = with pkgs; [
+            slurp
+            wf-recorder
+            ffmpeg
           ];
 
-          buildInputs = [
-          ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
+          # Common arguments can be set here to avoid repeating them later
+          # Note: changes here will rebuild all dependency crates
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+
+            nativeBuildInputs = [
+              pkgs.makeWrapper
+            ];
+
+            buildInputs = [
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
+          };
+
+          wlgif = craneLib.buildPackage (
+            commonArgs
+            // {
+              cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+              # Additional environment variables or build phases/hooks can be set
+              # here *without* rebuilding all dependency crates
+              # MY_CUSTOM_VAR = "some value";
+
+              # Wrap the binary to include runtime dependencies in PATH
+              postInstall = ''
+                wrapProgram $out/bin/wlgif \
+                  --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+              '';
+
+              meta = {
+                description = "Lightweight screen recorder for wlroots-based Wayland compositors that captures regions as GIFs";
+                homepage = "https://github.com/doprz/wlgif";
+                license = pkgs.lib.licenses.mit;
+                maintainers = pkgs.lib.maintainers.doprz;
+                platforms = pkgs.lib.platforms.unix;
+                mainProgram = "wlgif";
+              };
+            }
+          );
+        in
+        {
+          checks = {
+            inherit wlgif;
+          };
+
+          packages.default = wlgif;
+
+          apps.default = {
+            type = "app";
+            program = lib.getExe wlgif;
+          };
+
+          devShells.default = craneLib.devShell {
+            name = "wlgif-dev";
+            checks = self'.checks;
+
+            # Additional dev-shell environment variables can be set directly
+            # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+            # cargo and rustc are provided by default
+            packages = [
+            ]
+            ++ runtimeDeps;
+          };
         };
-
-        wlgif = craneLib.buildPackage (
-          commonArgs
-          // {
-            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-            # Additional environment variables or build phases/hooks can be set
-            # here *without* rebuilding all dependency crates
-            # MY_CUSTOM_VAR = "some value";
-
-            # Wrap the binary to include runtime dependencies in PATH
-            postInstall = ''
-              wrapProgram $out/bin/wlgif \
-                --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
-            '';
-
-            meta = {
-              description = "Lightweight screen recorder for wlroots-based Wayland compositors that captures regions as GIFs";
-              homepage = "https://github.com/doprz/wlgif";
-              license = pkgs.lib.licenses.mit;
-              maintainers = pkgs.lib.maintainers.doprz;
-              platforms = pkgs.lib.platforms.unix;
-              mainProgram = "wlgif";
-            };
-          }
-        );
-      in
-      {
-        checks = {
-          inherit wlgif;
-        };
-
-        packages.default = wlgif;
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = wlgif;
-        };
-
-        devShells.default = craneLib.devShell {
-          name = "wlgif-dev";
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # cargo and rustc are provided by default
-          packages = [
-          ]
-          ++ runtimeDeps;
-        };
-      }
-    );
+    };
 }
