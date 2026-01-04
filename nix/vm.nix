a9effee8f8b455ac214430cf2e7caf79b3d64322
doprz <52579214+doprz@users.noreply.github.com>
2026-01-03 22:03:19 -0600

    refactor(nix): spice module

--- nix/vm.nix
+++ nix/vm.nix
@@ -38,6 +38,7 @@
             };
             modules = [
               ./vm/base.nix
+              ./vm/spice.nix
               module
             ];
           };
--- nix/vm/base.nix
+++ nix/vm/base.nix
@@ -6,31 +6,11 @@
   networking.hostName = "wlgif-test-vm";
   networking.networkmanager.enable = true;
 
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
-
   virtualisation.vmVariant = {
     virtualisation.cores = 4;
     virtualisation.memorySize = 4096;
     virtualisation.diskSize = 4096;
     virtualisation.graphics = true;
-
-    # Use SPICE with QXL for resizable display
-    virtualisation.qemu.options = [
-      "-vga qxl"
-      # "-vga none"
-      # "-device virtio-vga"
-      "-display none"
-      "-device virtio-serial-pci"
-      # SPICE agent (for resize/clipboard)
-      "-chardev spicevmc,id=vdagent,debug=0,name=vdagent"
-      "-device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-      # QEMU guest agent (for host communication)
-      "-chardev socket,path=/tmp/qga.sock,server=on,wait=off,id=qga0"
-      "-device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0"
-      # SPICE server for remote-viewer connection
-      "-spice port=5930,disable-ticketing=on"
-    ];
   };
 
   system.stateVersion = "25.11";
