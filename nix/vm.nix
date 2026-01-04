6a4fa70205515b189dce6b0f78ea37ada645b93d
doprz <52579214+doprz@users.noreply.github.com>
2026-01-03 20:36:42 -0600

    feat(nix): add gnome VMs and virt-viewer via spice

--- nix/vm.nix
+++ nix/vm.nix
@@ -3,6 +3,7 @@
   perSystem =
     {
       self',
+      pkgs,
       lib,
       system,
       ...
@@ -26,10 +27,59 @@
           type = "app";
           program = lib.getExe vm.config.system.build.vm;
         };
+
+      runVMSpice =
+        module:
+        let
+          vm = inputs.nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = {
+              wlgif = self'.packages.default;
+            };
+            modules = [
+              ./vm/base.nix
+              module
+            ];
+          };
+
+          program = pkgs.writeShellScript "run-vm" ''
+            SHARED_DIR=$(pwd)
+            export SHARED_DIR
+
+            # Start qemu vm in the background
+            ${pkgs.lib.getExe vm.config.system.build.vm} "$@" & VM_PID=$!
+
+            # Wait for SPICE port to be available
+            echo "Waiting for SPICE server on port 5930..."
+            timeout=30
+            while ! ${pkgs.netcat}/bin/nc -z localhost 5930 2>/dev/null; do
+              sleep 0.5
+              timeout=$((timeout - 1))
+              if [ $timeout -le 0 ]; then
+                echo "Timeout waiting for SPICE port"
+                exit 1
+              fi
+            done
+
+            echo "Launching remote-viewer..."
+            ${pkgs.virt-viewer}/bin/remote-viewer spice://localhost:5930
+
+            # When remote-viewer closes, kill the VM
+            kill $VM_PID 2>/dev/null
+          '';
+        in
+        {
+          type = "app";
+          program = "${program}";
+        };
     in
     {
       apps = {
         sway-vm = runVM ./vm/sway.nix;
+        sway-vm-spice = runVMSpice ./vm/sway.nix;
+
+        gnome-vm = runVM ./vm/gnome.nix;
+        gnome-vm-spice = runVMSpice ./vm/gnome.nix;
       };
     };
 }
--- nix/vm/base.nix
+++ nix/vm/base.nix
@@ -6,11 +6,31 @@
   networking.hostName = "wlgif-test-vm";
   networking.networkmanager.enable = true;
 
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+
   virtualisation.vmVariant = {
-    virtualisation.cores = 2;
-    virtualisation.memorySize = 2048;
+    virtualisation.cores = 4;
+    virtualisation.memorySize = 4096;
     virtualisation.diskSize = 4096;
     virtualisation.graphics = true;
+
+    # Use SPICE with QXL for resizable display
+    virtualisation.qemu.options = [
+      "-vga qxl"
+      # "-vga none"
+      # "-device virtio-vga"
+      "-display none"
+      "-device virtio-serial-pci"
+      # SPICE agent (for resize/clipboard)
+      "-chardev spicevmc,id=vdagent,debug=0,name=vdagent"
+      "-device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+      # QEMU guest agent (for host communication)
+      "-chardev socket,path=/tmp/qga.sock,server=on,wait=off,id=qga0"
+      "-device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0"
+      # SPICE server for remote-viewer connection
+      "-spice port=5930,disable-ticketing=on"
+    ];
   };
 
   system.stateVersion = "25.11";
@@ -35,8 +55,18 @@
   services.xserver.enable = true;
   services.dbus.enable = true;
   services.libinput.enable = true;
-  services.qemuGuest.enable = true;
   security.polkit.enable = true;
+
+  # rtkit (optional, recommended) allows Pipewire to use the realtime scheduler for increased performance.
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  programs.fish.enable = true;
 
   users.users.wlgif-dev = {
     description = "wlgif dev user";
@@ -46,6 +76,6 @@
       "networkmanager"
     ];
     hashedPassword = "";
-    shell = pkgs.bash;
+    shell = pkgs.fish;
   };
 }
